260dd5e10ea98a155e3856ff0306a6d1875084f1
Jzapata <jhonny.z1022@gmail.com>
2021-12-06 17:03:59 -0500

    primer modelo y configuracion

---
+++ src/main/java/com/example/demo/models/UsuarioModel.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+public class UsuarioModel {
+
+    private Long id;
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+    
+    
+}
